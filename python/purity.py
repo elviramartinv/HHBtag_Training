415564cff8e38520374178904128acf3babfa322
Elvira <elvira.martin@ciemat.es>
2023-10-31 11:11:30 +0100

    output path changed

--- python/purity.py
+++ python/purity.py
@@ -4,15 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numba
 
+from statsmodels.stats.proportion import proportion_confint
+
 from Apply import ApplyTraining
 
-masses = [250, 500, 1000, 1500, 3000]
-path = "/afs/cern.ch/user/e/emartinv/public/Fork_Framework/Framework/training_skim/GluGluToRadionToHHTo2B2Tau_M-"
+# ggf and VBF res
+masses = [250, 260, 270, 280, 300, 320, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000]
+
+#ggf non res
+# masses = ['SM', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+#VBF non res
+
+path = "/afs/cern.ch/user/e/emartinv/public/Fork_Framework/Framework/training_skim/GluGluToBulkGravitonToHHTo2B2Tau_M-"
+# path = "/afs/cern.ch/user/e/emartinv/public/Fork_Framework/Framework/training_skim/GluGluToRadionToHHTo2B2Tau_M-"
+# path = "/afs/cern.ch/user/e/emartinv/public/Fork_Framework/Framework/training_skim/VBFToBulkGravitonToHHTo2B2Tau_M-"
+# path = "/afs/cern.ch/user/e/emartinv/public/Fork_Framework/Framework/training_skim/VBFToRadionToHHTo2B2Tau_M-"
+# path = "/afs/cern.ch/user/e/emartinv/public/Fork_Framework/Framework/training_skim/GluGluToHHTo2B2Tau_node_"
 
 taggers = ["particleNetAK4_B", "HHBtagScore", "btagDeepFlavB"]
 results = {}
 
-weights = 'ggf_model/model'
+weights = 'newHHmodel/model'
 training_variables = '../config/training_variables.json'
 params_json = '../config/params_optimized_training.json'
 
@@ -28,23 +41,25 @@ def count_matched(sorted_idx, genMatched):
             cnt += 1
     return cnt
 
-
 for mass in masses:
     file = path + str(mass) + ".root"
     file_results = {}
-
-    print("applying training")
+    error = {}
+    
     newHH_pred, genMatched = applier.apply(file, parity)
-    print("applied training")
     newHH_sorted = np.argsort(-newHH_pred)
     newHH_nMatched = count_matched(newHH_sorted, genMatched)
     purity = float(newHH_nMatched) / newHH_pred.shape[0]
-    file_results['newHH'] = purity
+    file_results['newHHBtag'] = purity
+    
+    lower_newHH, upper_newHH = proportion_confint(newHH_nMatched, newHH_pred.shape[0], alpha=0.68, method='beta')
+    file_results['newHHBtag_err'] = (purity - lower_newHH, upper_newHH - purity)
 
-    print(f'{mass} newHH_pred.shape={newHH_pred.shape} newHH_nMatched={newHH_nMatched} purity={purity}')
+    # print(f'{mass} newHH_pred.shape={newHH_pred.shape} newHH_nMatched={newHH_nMatched} purity={purity}')
 
     df = ROOT.RDataFrame("Event", file)
-    df = df.Filter("RecoJet_pt.size()>=2").Filter(f'event % 2 == {parity}')
+    df = df.Filter("RecoJet_pt.size()>=2").Filter(f'event % 2 == {parity}')    
+
     num_evt = df.Count()
     df = df.Define('RecoJet_idx', 'CreateIndexes(RecoJet_pt.size())')
     for tagger in taggers:
@@ -53,41 +68,66 @@ for mass in masses:
 
         num_matches = df.Filter(f"{tagger}_FirstTwoJetsMatched").Count()
         purity = float(num_matches.GetValue()) / num_evt.GetValue()
-        print(f'{mass} {tagger} num_matches={num_matches.GetValue()} num_evt={num_evt.GetValue()} purity={purity}')
+        # print(f'{mass} {tagger} num_matches={num_matches.GetValue()} num_evt={num_evt.GetValue()} purity={purity}')
 
-        file_results[tagger] = purity
-
-
+        # Calculate ci
+        lower, upper = proportion_confint(num_matches.GetValue(), num_evt.GetValue(), alpha=0.68, method='beta')
+        
+      
+        file_results[tagger] = {
+            'purity': purity,
+            'ci': (lower, upper),
+            'err': (purity - lower, upper-purity)
+        }
+        # print(f'CI (lower): {file_results[tagger]["ci"][0]}')
+        # print(f'CI (upper): {file_results[tagger]["ci"][1]}') 
+        # print(f'err (lower): {file_results[tagger]["err"][0]}')
+        # print(f'err (upper): {file_results[tagger]["err"][1]}')        
     results[mass] = file_results
 
-with open("purity_results.json", "w") as json_file:
+with open("../output/purity/purity_results.json", "w") as json_file:
     json.dump(results, json_file)
 
-# with open("purity_results.json", "r") as json_file:
-#     results = json.load(json_file)
 
 
-particleNet_purities = [result["particleNetAK4_B"] for result in results.values()]
-deepFlav_purities = [result["btagDeepFlavB"] for result in results.values()]
-HHBtag_purities = [result["HHBtagScore"] for result in results.values()]
-newHHbtag_purities = [result["newHH"] for result in results.values()]
 
-plt.figure(figsize=(10, 6))
-plt.plot(masses, particleNet_purities, marker='o', color='green', label='ParticleNet')
-plt.plot(masses, deepFlav_purities, marker='o', color='orange', label='DeepFlav')
-plt.plot(masses, HHBtag_purities, marker='o', color='red', label='HHBtag')
-plt.plot(masses,newHHbtag_purities, marker='o', color='black', label='newHH')
-plt.xlabel('Mass')
+particleNet_purities = [result["particleNetAK4_B"]["purity"] for result in results.values()]
+particleNet_err = [result["particleNetAK4_B"]["err"] for result in results.values()]
+deepFlav_purities = [result["btagDeepFlavB"]["purity"] for result in results.values()]
+deepFlav_err = [result["btagDeepFlavB"]["err"] for result in results.values()]
+HHBtag_purities = [result["HHBtagScore"]["purity"] for result in results.values()]
+HHBtag_err = [result["HHBtagScore"]["err"] for result in results.values()]
+newHHbtag_purities = [result["newHHBtag"] for result in results.values()]
+newHHbtag_err = [result["newHHBtag_err"] for result in results.values()]
+
+xtick_locations = np.arange(len(masses))
+
+fig = plt.figure(figsize=(10, 6))
+ay = plt.gca()
+# plt.scatter(xtick_locations, particleNet_purities, marker='o', color='green', label='ParticleNet')
+ay.errorbar(xtick_locations, particleNet_purities, yerr=np.array(particleNet_err).T, fmt='o', color='green', label='ParticleNet')
+# plt.scatter(xtick_locations, deepFlav_purities, marker='o', color='orange', label='DeepFlav')
+ay.errorbar(xtick_locations, deepFlav_purities, yerr=np.array(deepFlav_err).T, fmt='o', color='orange', label='DeepFlav')
+# plt.scatter(xtick_locations, HHBtag_purities, marker='o', color='red', label='HHBtag')
+ay.errorbar(xtick_locations, HHBtag_purities, yerr=np.array(HHBtag_err).T, fmt='o', color='red', label='oldHHBtag')
+# plt.scatter(xtick_locations,newHHbtag_purities, marker='o', color='black', label='newHHBtag')
+ay.errorbar(xtick_locations, newHHbtag_purities, yerr=np.array(newHHbtag_err).T, fmt='o', color='black', label='newHHBtag')
+
+plt.xticks(xtick_locations, masses, rotation=45)
+
+plt.xlabel('mass [GeV]')
 plt.ylabel('Purity')
-plt.title('Purity vs Mass for Different Taggers')
+plt.title('ggF spin-2')
 plt.legend()
-plt.grid()
+# plt.grid()
+plt.tight_layout()
 
-
-plt.savefig('purity_plot.pdf')
+plt.savefig('../output/purity/purity_ggF_spin2.pdf')
 
 
 #plt.show()
+
+
 
 
 
