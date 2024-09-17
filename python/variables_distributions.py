820225367136eeb7d00502fa4742f7006384824d
Elvira <elvira.martin@ciemat.es>
2024-09-17 11:47:03 +0200

    script for input variables

--- python/variables_distributions.py
+++ python/variables_distributions.py
@@ -24,10 +24,10 @@ df = df.Define('n_jets', 'RecoJet_pt.size()') \
        .Define('rel_met_pt_htt_pt', 'MET_pt / htt_scalar_pt') \
        .Define('htt_met_dphi', 'TVector2::Phi_mpi_pi(MET_phi - htt_p4.Phi())') 
 
-taggers = ["DeepFlavB", "PNetB", "RobustParTAK4B"]
+taggers = ["btagDeepFlavB", "btagPNetB", "btagRobustParTAK4B", "HHBtagScore"]
 for tag in taggers:
-    df = df.Define(f'{tag}_leading_jet_idx', f'ArgMax(RecoJet_btag{tag})') \
-           .Define(f'{tag}_subleading_jet_idx', f'ArgMax(RecoJet_btag{tag}[RecoJet_btag{tag} != RecoJet_btag{tag}[{tag}_leading_jet_idx]])') \
+    df = df.Define(f'{tag}_leading_jet_idx', f'ArgMax(RecoJet_{tag})') \
+           .Define(f'{tag}_subleading_jet_idx', f'ArgMax(RecoJet_{tag}[RecoJet_{tag} != RecoJet_{tag}[{tag}_leading_jet_idx]])') \
            .Define(f'{tag}_lead_jet_valid', f'{tag}_leading_jet_idx < RecoJet_pt.size()') \
            .Define(f'{tag}_lead_jet_p4', f'LorentzVectorM(RecoJet_pt[{tag}_leading_jet_idx], RecoJet_eta[{tag}_leading_jet_idx], RecoJet_phi[{tag}_leading_jet_idx], RecoJet_mass[{tag}_leading_jet_idx])') \
            .Define(f'{tag}_lead_jet_pt', f'RecoJet_pt[{tag}_leading_jet_idx]') \
@@ -40,8 +40,8 @@ for tag in taggers:
            .Define(f'{tag}_lead_jet_genbJet', f'RecoJet_genMatched[{tag}_leading_jet_idx]') \
            .Define(f'{tag}_lead_jet_htt_dphi', f'TVector2::Phi_mpi_pi(htt_p4.Phi() - {tag}_lead_jet_p4.Phi())') \
            .Define(f'{tag}_lead_jet_htt_deta', f'htt_p4.Eta() - {tag}_lead_jet_p4.Eta()') \
-           .Define(f'{tag}_lead_jet_{tag}', f'RecoJet_btag{tag}[{tag}_leading_jet_idx]') \
-           .Define(f'{tag}_sublead_jet_{tag}', f'RecoJet_btag{tag}[{tag}_subleading_jet_idx]')
+           .Define(f'{tag}_lead_jet_{tag}', f'RecoJet_{tag}[{tag}_leading_jet_idx]') \
+           .Define(f'{tag}_sublead_jet_{tag}', f'RecoJet_{tag}[{tag}_subleading_jet_idx]')
 
 # Dibujar las distribuciones
 variables = ["htt_pt", "htt_eta", "htt_met_dphi", "rel_met_pt_htt_pt", "htt_scalar_pt"]
@@ -74,8 +74,12 @@ for var in variables:
     # canvas.SaveAs(f"{output_dir}/{var}_{input_file}.png")
 
 # Guardar los histogramas en un archivo
-output_file = ROOT.TFile(f"{output_dir}/histograms_{input_file}.root", "RECREATE")
+output_file = ROOT.TFile(f"{output_dir}/histograms_{input_file}", "RECREATE")
 for var in variables:
     hist = df.Histo1D(var)
     hist.Write()
+
+
+tree = df.Snapshot("tree", f"{output_dir}/tree_{input_file}.root", [f'{tag}_lead_jet_{tag}' for tag in taggers] + [f'{tag}_sublead_jet_{tag}' for tag in taggers])
+
 output_file.Close()
